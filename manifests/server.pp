6874df62afd215b1adfa98d9b65aa85469e545d3
Christopher Johnson <cjohn@ceh.ac.uk>
2014-03-26 14:56:58 +0000

    Chaining all resources and realizing all nagios types

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,17 +1,20 @@
 class nagios::server(
-  $users_file = '/etc/nagios3/htpasswd.users'
+  $version         = installed,
+  $nrpe_version    = installed,
+  $plugins_version = installed,
+  $users_file      = '/etc/nagios3/htpasswd.users'
 ) {
   package { 'nagios3' :
-    ensure => installed,
+    ensure => $version,
   }
 
   package { 'nagios-nrpe-plugin' :
-    ensure => installed,
+    ensure => $nrpe_version,
     before => Service['nagios3'],
   }
 
   package { 'nagios-plugins' :
-    ensure => installed,
+    ensure => $plugins_version,
     before => Service['nagios3'],
   }
 
@@ -40,12 +43,34 @@ class nagios::server(
   }
   
   # Gather the local resources
-  Package['nagios3'] -> Nagios_host <||>    ~> Service['nagios3']
-  Package['nagios3'] -> Nagios_service <||> ~> Service['nagios3']
-  Package['nagios3'] -> Nagios_command <||> ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_command <||>           ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_contact <||>           ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_contactgroup <||>      ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_host <||>              ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_hostdependency <||>    ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_hostescalation <||>    ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_hostextinfo <||>       ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_hostgroup <||>         ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_service <||>           ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_servicedependency <||> ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_serviceescalation <||> ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_serviceextinfo <||>    ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_serviceextgroup <||>   ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_timeperiod <||>        ~> Service['nagios3']
 
   # Gather the exported nagios resources 
-  Package['nagios3'] -> Nagios_host <<||>>        ~> Service['nagios3']
-  Package['nagios3'] -> Nagios_service <<||>>     ~> Service['nagios3']
-  Package['nagios3'] -> Nagios_hostextinfo <<||>> ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_command <<||>>           ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_contact <<||>>           ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_contactgroup <<||>>      ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_host <<||>>              ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_hostdependency <<||>>    ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_hostescalation <<||>>    ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_hostextinfo <<||>>       ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_hostgroup <<||>>         ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_service <<||>>           ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_servicedependency <<||>> ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_serviceescalation <<||>> ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_serviceextinfo <<||>>    ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_serviceextgroup <<||>>   ~> Service['nagios3']
+  Package['nagios3'] -> Nagios_timeperiod <<||>>        ~> Service['nagios3']
 }
