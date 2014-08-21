e6a4d817161b11c9ada619a8f859ba189d02f2ea
Christopher Johnson <cjohn@ceh.ac.uk>
2014-08-21 16:01:40 +0100

    Typo with host performance name

--- manifests/command_hostperformance.pp
+++ manifests/command_hostperformance.pp
@@ -8,7 +8,7 @@
 # Christopher Johnson - cjohn@ceh.ac.uk
 #
 class nagios::command_hostperformance {
-  nagios_command { 'process-service-perfdata':
+  nagios_command { 'process-host-perfdata':
     command_line => '/usr/lib/pnp4nagios/libexec/process_perfdata.pl --bulk=/tmp/host-perfdata',
   }
 }
