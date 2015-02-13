00789192816d0535a0b6eb6773a890395aa0a196
Christopher Johnson <cjohn@ceh.ac.uk>
2015-02-13 14:59:23 +0000

    Allowing notifications to be disabled

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -6,6 +6,7 @@
 #
 # [*command*] The command to run
 # [*service_description*] A description of this nrpe service
+# [*notifications_enabled*] If nagios should notify when this command fails
 # [*notification_period*] The notification period of this nrpe service
 # [*host_name*] The host name which this nagios service can be contacted on.
 # [*command_name*] The name of the command
@@ -21,10 +22,11 @@
 define nagios::nrpe (
   $command,
   $service_description,
-  $notification_period = $nagios::check::default_notification_period,
-  $host_name           = $fqdn,
-  $command_name        = $name,
-  $use                 = 'generic-service'
+  $notifications_enabled = undef,
+  $notification_period   = $nagios::check::default_notification_period,
+  $host_name             = $fqdn,
+  $command_name          = $name,
+  $use                   = 'generic-service'
 ) {
   if ! defined(Class['::nagios::client']) {
     fail('You must include the nagios::client class before defining a nagios::nrpe')
@@ -36,10 +38,11 @@ define nagios::nrpe (
   }
 
   @@nagios_service { "${command_name}_${fqdn}" :
-    check_command       => "check_nrpe_1arg!${command_name}",
-    use                 => $use,
-    host_name           => $host_name,
-    notification_period => $notification_period,
-    service_description => $service_description,
+    check_command         => "check_nrpe_1arg!${command_name}",
+    use                   => $use,
+    host_name             => $host_name,
+    notification_period   => $notification_period,
+    service_description   => $service_description,
+    notifications_enabled => $notifications_enabled,
   }
 }
