dad2e55abe0ff035fa701ef404de5b7230274555
Christopher Johnson <cjohn@ceh.ac.uk>
2015-02-13 13:56:06 +0000

    Adding a plugin.pp which will allow us to easily deploy extra checks to servers

---
+++ manifests/plugin.pp
@@ -0,0 +1,25 @@
+# == Define: nrpe::plugin
+#
+# Deploy a nagios plugin to deploy to the standard nagios plugins directory for
+# the current kernel
+#
+# === Parameters
+#
+# [*source*]  location where the plugin should be deployed from
+# [*command*] name of the command which the plugin should be deployed as
+# [*path]     where the plugin should be deployed to
+#
+# === Authors
+#
+# Christopher Johnson - cjohn@ceh.ac.uk
+#
+define nagios::plugin (
+  $source,
+  $command      = $name,
+  $path = $::nagios::plugins_path,
+) {
+  file { "${path}/${command}":
+    mode   => '755',
+    source => $source,
+  }
+}
